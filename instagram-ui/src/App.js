8ba97e1c99a277e3d21041524105998aa92d68b0
raz <raz2222@hotmail.com>
2020-06-17 17:09:36 +0300

    first

--- instagram-ui/src/App.js
+++ instagram-ui/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-
-import "./App.css";
-
+import "./App.scss";
+import Header from "./Header/Header";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
-
 export default App;
